8b05baa0333259d2a64fc831fa59b1a02f2032ac
4uad <dmamadrid@gmail.com>
2021-02-18 20:05:55 +0100

    Updates

---
+++ precache-manifest.d5898d0ce0c6eb57ef2090d01f8526ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2f48aa50aec56253f83c4a79657846",
+    "url": "/snakejs/index.html"
+  },
+  {
+    "revision": "9a42ebb9ec1dd426bae1",
+    "url": "/snakejs/static/css/main.c3c1cde3.chunk.css"
+  },
+  {
+    "revision": "69992849cf64c8fe7b74",
+    "url": "/snakejs/static/js/2.ee292406.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snakejs/static/js/2.ee292406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a42ebb9ec1dd426bae1",
+    "url": "/snakejs/static/js/main.b4e34274.chunk.js"
+  },
+  {
+    "revision": "7284531477f70d73675b",
+    "url": "/snakejs/static/js/runtime-main.8d9bdb68.js"
+  },
+  {
+    "revision": "ba9166f5d60117576a92500b0dc578e8",
+    "url": "/snakejs/static/media/wasd.ba9166f5.png"
+  }
+]);
